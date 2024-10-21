debc9ecb2be5ee3adc94459553a2204a102deb87
Aanasta <anastasia.gnes@gmail.com>
2024-10-21 18:32:12 +0200

    updating Record builder and adding error processing

--- src/main/java/pageobjects/DataTable.java
+++ src/main/java/pageobjects/DataTable.java
@@ -4,6 +4,7 @@ import models.Record;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,21 +12,27 @@ import utils.ui.ScrollingUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class DataTable extends AbstractPage {
 
     private static final Logger logger = LogManager.getLogger(DataTable.class);
-
-    @FindBy(xpath = "//tr[@class='row']")
-    private List<WebElement> recordsRows;
-    @FindBy(className = "header")
-    private WebElement header;
 
     private static final String COMPANY_NAME_BY_INDEX_XPATH = "//tr[@class='row'][%s]/td[1]";
     private static final String TICKER_BY_INDEX_XPATH = "//tr[@class='row'][%s]/td[2]";
     private static final String COB_DATE_BY_INDEX_XPATH = "//tr[@class='row'][%s]/td[3]";
     private static final String STOCK_PRICE_BY_INDEX_XPATH = "//tr[@class='row'][%s]/td[4]";
     private static final String MARKET_CAP_BY_INDEX_XPATH = "//tr[@class='row'][%s]/td[5]";
+
+    private static final String TICKER_FIELD = "Ticker";
+    private static final String COB_DATE_FIELD = "COB Date";
+    private static final String STOCK_PRICE_FIELD = "Stock Price";
+    private static final String MARKET_CAP_FIELD = "Market Cap";
+
+    @FindBy(xpath = "//tr[@class='row']")
+    private List<WebElement> recordsRows;
+    @FindBy(className = "header")
+    private WebElement header;
 
     public DataTable(WebDriver driver) {
         super(driver);
@@ -36,19 +43,60 @@ public class DataTable extends AbstractPage {
         logger.info("Found {} records in the data table", count);
         List<Record> records = new ArrayList<>();
         for (int i = 1; i <= count; i++) {
-            String companyName = driver.findElement(By.xpath(String.format(COMPANY_NAME_BY_INDEX_XPATH, i))).getText();
+            String companyName = getCompanyNameByIndex(i);
             logger.info("{}. Collecting data for the record: {} ", i, companyName);
             Record currentRecord = Record.builder()
                     .companyName(companyName)
-                    .ticker(driver.findElement(By.xpath(String.format(TICKER_BY_INDEX_XPATH, i))).getText())
-                    .cobDate(driver.findElement(By.xpath(String.format(COB_DATE_BY_INDEX_XPATH, i))).getText())
-                    .stockPrice(driver.findElement(By.xpath(String.format(STOCK_PRICE_BY_INDEX_XPATH, i))).getText())
-                    .marketCap(driver.findElement(By.xpath(String.format(MARKET_CAP_BY_INDEX_XPATH, i))).getText())
+                    .ticker(getTextValueByIndex(TICKER_BY_INDEX_XPATH, i, TICKER_FIELD, companyName).orElse(""))
+                    .cobDate(getTextValueByIndex(COB_DATE_BY_INDEX_XPATH, i, COB_DATE_FIELD, companyName).orElse(""))
+                    .stockPrice(getStockPriceByIndex(i, companyName))
+                    .marketCap(getMarketCapByIndex(i, companyName))
                     .build();
             records.add(currentRecord);
         }
         logger.info("Collected {} records from the data table", records.size());
         return records;
+    }
+
+    private String getCompanyNameByIndex(int i) {
+        try {
+            return driver.findElement(By.xpath(String.format(COMPANY_NAME_BY_INDEX_XPATH, i))).getText();
+        } catch (NoSuchElementException e) {
+            String composedName = String.format("Company %d", i);
+            logger.error("Could not find Company Name for company with index {}. Composing name: {}", i, composedName);
+            return composedName;
+        }
+    }
+
+    private Optional<String> getTextValueByIndex(String locator, int i, String fieldName, String companyName) {
+        try {
+            String text = driver.findElement(By.xpath(String.format(locator, i))).getText();
+            return Optional.ofNullable(text);
+        } catch (NoSuchElementException e) {
+            logger.error("Could not find {} for Company {}. Returning empty string", fieldName, companyName);
+            return Optional.empty();
+        }
+    }
+
+    private double getStockPriceByIndex(int i, String companyName) {
+        return getNumericValueByIndex(STOCK_PRICE_BY_INDEX_XPATH, i, STOCK_PRICE_FIELD,  companyName)
+                .orElse(0.0);
+    }
+
+    private int getMarketCapByIndex(int i, String companyName) {
+        return getNumericValueByIndex(MARKET_CAP_BY_INDEX_XPATH, i, MARKET_CAP_FIELD, companyName)
+                .map(Double::intValue)
+                .orElse(0);
+    }
+
+    private Optional<Double> getNumericValueByIndex(String locator, int i, String fieldName, String companyName) {
+        try {
+            String value = driver.findElement(By.xpath(String.format(locator, i))).getText();
+            return Optional.of(Double.parseDouble(value));
+        } catch (NoSuchElementException | NumberFormatException e) {
+            logger.error("Could not collect {} for Company {}. Returning empty value", fieldName, companyName);
+            return Optional.empty();
+        }
     }
 
     private int getAllRecordsCount() {
